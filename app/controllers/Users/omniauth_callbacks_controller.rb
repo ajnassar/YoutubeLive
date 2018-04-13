5bbf16b6fdf71760b07de60cb88e8ae1e28646f2
ajnassar <ajnassar@gmail.com>
2018-04-13 16:01:22 -0700

    Setup omniauth to work

--- app/controllers/Users/omniauth_callbacks_controller.rb
+++ app/controllers/Users/omniauth_callbacks_controller.rb
@@ -1,15 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
-    user = ::User.from_omniauth(oauth_response)
+    # byebug
+    user = ::User.from_omniauth(request.env["omniauth.auth"])
 
     if user.persisted?
-      flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: provider)
+      flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: request.env["omniauth.auth"].provider)
       sign_in_and_redirect user, event: :authentication
     else
       session["devise.google_data"] = oauth_response.except(:extra)
       params[:error] = :account_not_found
-      do_failure_things
+      # do_failure_things
     end
   end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,2 @@
 class HomesController < ApplicationController
-  before_action :authenticate_user!
 end
