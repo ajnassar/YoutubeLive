3b643fc3acb158a8f0d3951a61582c0085972301
ajnassar <ajnassar@gmail.com>
2018-04-17 23:17:50 -0700

    Add action to fetch broadcasts from youtube

--- app/javascript/youtube_live/actions.jsx
+++ app/javascript/youtube_live/actions.jsx
@@ -4,3 +4,13 @@ export const receiveBroadcasts = broadcasts => ({
   type: RECEIVE_BROADCASTS,
   broadcasts
 })
+
+export function getLiveBroadcasts(searchTerm) {
+  return function action(dispatch, getState) {
+    fetch(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&maxResults=25&part=snippet&key=api-key-goes-here`).then(results => {
+      return results.json();
+    }).then(data => {
+      dispatch(RECEIVE_BROADCASTS(broadcasts))
+    })
+  }
+}
